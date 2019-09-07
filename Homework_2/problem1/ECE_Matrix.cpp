6c5ad2be106bb919daca2fa5df5bf066c11cfc4e
Alexander Agudelo <awagud12@gmail.com>
2019-09-07 14:11:49 -0400

    created class

---
+++ Homework_2/problem1/ECE_Matrix.cpp
@@ -0,0 +1,8 @@
+/*
+Author: Alex Agudelo
+Class: ECE 6122
+Last date modified: 9/7/19
+Description: 
+*/
+
+#include "ECE_Matrix.hpp"
---
+++ Homework_2/problem1/ECE_Matrix.hpp
@@ -0,0 +1,17 @@
+/*
+Author: Alex Agudelo
+Class: ECE 6122
+Last date modified: 9/7/19
+Description: 
+*/
+
+#ifndef HOMEWORK_2_ECE_MATRIX_HPP
+#define HOMEWORK_2_ECE_MATRIX_HPP
+
+
+class ECE_Matrix {
+
+};
+
+
+#endif //HOMEWORK_2_ECE_MATRIX_HPP
