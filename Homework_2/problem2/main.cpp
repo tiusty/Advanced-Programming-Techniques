cfc2e06f5e247f44e75cc1990d30f1dd485a2f4c
Alex Agudelo <awagud12@gmail.com>
2019-09-10 09:37:35 -0400

    Add main file

---
+++ Homework_2/problem2/main.cpp
@@ -0,0 +1,13 @@
+/*
+Author: Alex Agudelo
+Class: ECE 6122
+Last date modified: 9/10/2019
+Description:
+ Main file for problem 2 homework 2. Executes the program
+*/
+
+int main()
+{
+    return 0;
+}
+
