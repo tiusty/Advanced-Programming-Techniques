efefd8c530dc0f3004e2caac07b7fecea79cc8e1
Alex Agudelo <awagud12@gmail.com>
2019-09-25 09:37:39 -0400

    Added more error checks

--- Homework_3/problem1/lattice.cpp
+++ Homework_3/problem1/lattice.cpp
@@ -26,9 +26,18 @@ unsigned int Lattice::numLatNodes()
 unsigned int Lattice::getNode(unsigned int row, unsigned int col)
 {
     int rowSigned = row;
+    // Row and col are base 1
     if(row < 1 or col < 1)
     {
         throw std::out_of_range("Row and col must be greater than 0");
+    }
+    if(col < row)
+    {
+        throw std::out_of_range("This index does not exist since we are only storing top half of matrix");
+    }
+    if(row > latHeight || col > latWidth)
+    {
+        throw std::out_of_range("The desired index is out of range for current lattice");
     }
 
     // In the first row case, the row-2 will become -1 which will still equate to 0, so this still works. Though
@@ -36,6 +45,8 @@ unsigned int Lattice::getNode(unsigned int row, unsigned int col)
 
     // Given that we are only storing the top half of the wide matrix (or square) we need to easily retrieve
     //  the node corresponding to the index we want.
-    return ((rowSigned-1)*latWidth - ((rowSigned-2)*(rowSigned-2) + (rowSigned-2))/2) + (col - 1 - (row - 1));
+    return ((rowSigned - 1) * latWidth -   // Add nodes from rows above
+            ((rowSigned - 2) * (rowSigned - 2) +  (rowSigned - 2)) / 2) + // Due to symmetry get rid of extra nodes that don't exist
+           (col - 1 - (row - 1)); // Adjust due to column but also since first node is the diagonal element, shift more
 }
 
