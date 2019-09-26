daa3569611ec17a1071ce4fa217c36908a57b750
Alex Agudelo <awagud12@gmail.com>
2019-09-25 20:02:19 -0400

    Added command line arguments

--- Homework_3/problem1/main.cpp
+++ Homework_3/problem1/main.cpp
@@ -9,10 +9,45 @@ Description:
 #include "lattice.h"
 
 #include <iostream>
+#include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    Lattice lat(4,10);
-    lat.findNumberOfPaths();
+    int opt;
+    int height{0};
+    int width{0};
+    bool badArgs{false};
+
+    // put ':' in the starting of the
+    // string so that program can
+    //distinguish between '?' and ':'
+    while((opt = getopt(argc, argv, "w:h:")) != -1)
+    {
+        switch(opt)
+        {
+            case 'h':
+                printf("height: %s\n", optarg);
+                height = std::stoi(optarg);
+                printf("option needs a value\n");
+                break;
+            case 'w':
+                printf("width: %s\n", optarg);
+                width = std::stoi(optarg);
+                printf("option needs a value\n");
+                break;
+            case '?':
+                printf("unknown option: %c\n", optopt);
+                badArgs = true;
+                break;
+        }
+    }
+
+    if (not badArgs)
+    {
+        Lattice lat(height,width);
+        lat.findNumberOfPaths();
+    } else {
+        std::cerr << "Bad options specified" << std::endl;
+    }
     return 1;
 }
