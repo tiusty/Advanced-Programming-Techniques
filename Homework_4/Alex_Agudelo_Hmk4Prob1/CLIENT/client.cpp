fcdd2b18f8b849006164547d736204dfdee2a735
Alexander Agudelo <awagud12@gmail.com>
2019-10-10 15:12:19 -0400

    Adding base code

---
+++ Homework_4/Alex_Agudelo_Hmk4Prob1/CLIENT/client.cpp
@@ -0,0 +1,8 @@
+/*
+Author: Alex Agudelo
+Class: ECE 6122
+Last date modified: 10/10/19
+Description: 
+*/
+
+#include "client.hpp"
---
+++ Homework_4/Alex_Agudelo_Hmk4Prob1/SERVER/server.hpp
@@ -0,0 +1,17 @@
+/*
+Author: Alex Agudelo
+Class: ECE 6122
+Last date modified: 10/10/19
+Description: 
+*/
+
+#ifndef HOMEWORK_4_SERVER_HPP
+#define HOMEWORK_4_SERVER_HPP
+
+
+class server {
+
+};
+
+
+#endif //HOMEWORK_4_SERVER_HPP
