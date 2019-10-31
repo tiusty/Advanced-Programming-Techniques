e6995fdef1d0aca2b0e8aa7708047971562e666c
Alexander Agudelo <awagud12@gmail.com>
2019-10-31 18:40:56 -0400

    More comments

--- Homework_5/Alex_Agudelo_Hmk5Prob1/ship.cpp
+++ Homework_5/Alex_Agudelo_Hmk5Prob1/ship.cpp
@@ -1,8 +1,9 @@
 /*
 Author: Alex Agudelo
 Class: ECE 6122
-Last date modified: 10/29/19
-Description: 
+Last date modified: 10/31/19
+Description:
+ Implements ship functionality
 */
 
 #include "ship.hpp"
@@ -11,6 +12,7 @@ Description:
 
 Coordinate Ship::getVelUnitVec()
 {
+    // Unit vec is the velocity component divided by the magniuted
     if(getMagVel() != 0)
     {
         return {velocity.x/getMagVel(), velocity.y/getMagVel(), velocity.z/getMagVel()};
@@ -20,6 +22,7 @@ Coordinate Ship::getVelUnitVec()
 
 Coordinate Ship::getDistUnitVec()
 {
+    // Unit vec is the velocity component divided by the magniuted
     if(getMagPos() != 0)
     {
         return {position.x/getMagPos(), position.y/getMagPos(), position.z/getMagPos()};
@@ -29,26 +32,33 @@ Coordinate Ship::getDistUnitVec()
 
 double Ship::getDistance(double pos, double dest)
 {
+    // L2 norm between two points in 1D
     return std::sqrt(std::pow(pos-dest,2));
 }
 
 double Ship::getDistance(Coordinate dest)
 {
+    // L2 norm between two points in 3D
     return std::sqrt(std::pow(position.x - dest.x, 2) + std::pow(position.y - dest.y, 2) + std::pow(position.z - dest.z, 2));}
 
 double Ship::getMagVel()
 {
+    // Norm of the velocity vector
     return std::sqrt(std::pow(velocity.x,2) + std::pow(velocity.y,2) + std::pow(velocity.z, 2));
 }
 
 double Ship::getMagPos()
 {
+    // Norm of the position vector
     return std::sqrt(std::pow(position.x,2) + std::pow(position.y,2) + std::pow(position.z, 2));
 }
 
 double Ship::forceToGetVel(double startVel, double endVel)
 {
+    // Equation of motion to for desired force
     double forceToGetVel = (endVel - startVel)*yellowJacketMass;
+
+    // Set bounds to prevent over powering if added to another force before calling setForce
     if(forceToGetVel > maxForce)
     {
         return maxForce;
--- Homework_5/Alex_Agudelo_Hmk5Prob1/ship.hpp
+++ Homework_5/Alex_Agudelo_Hmk5Prob1/ship.hpp
@@ -1,44 +1,115 @@
 /*
 Author: Alex Agudelo
 Class: ECE 6122
-Last date modified: 10/29/19
-Description: 
+Last date modified: 10/31/19
+Description:
+ Stores the data/position of a given ship in the world
 */
 
 #ifndef HOMEWORK_5_SHIP_HPP
 #define HOMEWORK_5_SHIP_HPP
 
+/**
+ * The mass of a yellow jacket
+ */
 constexpr int yellowJacketMass = 10000;
 
+/**
+ * Store coordinate as a struct for easy manipulation
+ */
 struct Coordinate
 {
+    // 3D positions
     double x;
     double y;
     double z;
 
+    /**
+     * Zero ctro
+     */
     Coordinate():Coordinate(0,0,0) {}
+    /**
+     * Set values for a Coordinate
+     */
     Coordinate(double xNew, double yNew, double zNew)
             :x(xNew), y(yNew), z(zNew)
     {}
 };
 
-
+/**
+ * Stores position/velocity/force information for a given ship
+ */
 class Ship {
 public: // Methods
+    /**
+     * Get the distance in 1-D line between two points
+     * @param pos Position 1
+     * @param dest Position 2
+     * @return The distance
+     */
     double getDistance(double pos, double dest);
+    /**
+     * Get the 3D distance between two 3D points.
+     * The distance is between this ship and the desired location
+     * @param dest The Coordinate of the other object
+     * @return The distance as an L2 norm
+     */
     double getDistance(Coordinate dest);
+    /**
+     * calculates the force needed to hit a target velocity in one second time step
+     * @param startVel The current vel
+     * @param endVel The desired vel
+     * @return The force needed
+     */
     double forceToGetVel(double startVel, double endVel);
+    /**
+     * Gets the Mag of the velocity
+     * @return The mag of the velocity
+     */
     double getMagVel();
+    /**
+     * Gets the mag of the position
+     * @return The mag of the position
+     */
     double getMagPos();
+    /**
+     * Gets the velocity unit vector of this ship
+     * @return The unit vector
+     */
     Coordinate getVelUnitVec();
+    /**
+     * Gets the position unit vector of this ship
+     * @return The unit vector
+     */
     Coordinate getDistUnitVec();
 
 public: // Members
+    /**
+     * The id of the ship
+     */
     int id{0};
+    /**
+     * The 3D position of the ship
+     */
     Coordinate position{0,0,0};
+    /**
+     * The 3D velocity of the ship
+     */
     Coordinate velocity{10,0,0};
+    /*
+     * The 3D force vector of this ship
+     */
     Coordinate force{0,0,0};
+    /*
+     * The max allowed force
+     */
     double maxForce{0};
+    /**
+     * State of the ship
+     *  0 = destroyed
+     *  1 = active
+     *  2 = docked
+     */
     int status{1};
 };
 
