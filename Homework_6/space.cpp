359f18c8021e48b51672f8b1800c194bdde80e7c
Alex Agudelo <awagud12@gmail.com>
2019-11-13 22:11:03 -0500

    Added nice queen

--- Homework_6/space.cpp
+++ Homework_6/space.cpp
@@ -51,6 +51,10 @@ void Space::draw()
     }
     else if(type == Piece::queen)
     {
+        setColor();
+        glTranslatef(-0.1,.15,0);
+        glScalef(.9, .9, .75);
+        glRotatef(90,1,0,0);
         drawQueen();
     }
     else if(type == Piece::king)
@@ -399,14 +403,144 @@ void Space::drawBishop()
 
 void Space::drawQueen()
 {
-
+    float x=0,y=0;
+    float scale = 3.72*0.75;
+    GLUquadricObj* object;
+    object = gluNewQuadric();
+    gluQuadricDrawStyle(object, GLU_FILL);
+    gluQuadricNormals(object, GL_SMOOTH);
+    gluQuadricTexture(object, GL_TRUE);
+    //A-AA
     glPushMatrix();
-    setColor();
-    glTranslatef(0,0,.7);
-    glRotatef(50, 0,.5,.5);
-    glutSolidTetrahedron();
+    glTranslatef((x+.5)/4.0, 6.82/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .17/scale, 0, .18/scale, 10, 10);
     glPopMatrix();
-}
+    //AA-B
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 6.54/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .23/scale, .17/scale, .28/scale, 10, 10);
+    glPopMatrix();
+    //B-C
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 6.40/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .72/scale, .23/scale, .14/scale, 10, 10);
+    glPopMatrix();
+    //C-D
+    //change the direction of the normals because we'll actually be seeing the inside of the cylinder.
+    gluQuadricOrientation(object, GLU_INSIDE);
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 6.40/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .86/scale, .72/scale, .14/scale, 10, 10);
+    glPopMatrix();
+    gluQuadricOrientation(object, GLU_OUTSIDE);
+    //D-E(disk)
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 6.54/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluDisk(object, .86/scale, 1.06/scale, 10, 10);
+    glPopMatrix();
+    //E-F
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 6.05/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .72/scale, 1.06/scale, .49/scale, 10, 10);
+    glPopMatrix();
+    //F-G
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 5.22/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .72/scale, .83/scale, .83/scale, 15, 15);
+    glPopMatrix();
+    //G-H
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 5.12/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .87/scale, .72/scale, .1/scale, 10, 10);
+    glPopMatrix();
+    //H-I
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 5.02/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .72/scale, .87/scale, .1/scale, 10, 10);
+    glPopMatrix();
+    //I-J
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 4.69/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .9/scale, .72/scale, .33/scale, 10, 10);
+    glPopMatrix();
+    //J-K
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 4.16/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, 1.39/scale, .9/scale, .53/scale, 10, 10);
+    glPopMatrix();
+    //K-L
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 4.08/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, 1.28/scale, 1.39/scale, .08/scale, 10, 10);
+    glPopMatrix();
+    //L-M
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 3.97/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, .69/scale, 1.28/scale, .11/scale, 10, 10);
+    glPopMatrix();
+    //M-N
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 1.81/scale, (y+.5)/4.0);
+    glRotatef(90,  -1, 0, 0);
+    gluCylinder(object, 1.04/scale, .69/scale, 2.16/scale, 20, 10);
+    glPopMatrix();
+    //N-O (Disk)
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 1.81/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluDisk(object, 1.04/scale, 1.18/scale, 10, 10);
+    glPopMatrix();
+    //O-P
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 1.21/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, 1.18/scale, 1.18/scale, .6/scale, 10, 10);
+    glPopMatrix();
+    //P-Q
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, .6/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, 1.59/scale, 1.18/scale, .61/scale, 10, 10);
+    glPopMatrix();
+    //Q-R
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, .37/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, 1.46/scale, 1.59/scale, .23/scale, 10, 10);
+    glPopMatrix();
+    //R-S
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, .31/scale, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, 1.59/scale, 1.46/scale, .06/scale, 10, 10);
+    glPopMatrix();
+    //S-T
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 0.0, (y+.5)/4.0);
+    glRotatef(90, -1, 0, 0);
+    gluCylinder(object, 1.59/scale, 1.59/scale, .31/scale, 10, 10);
+    glPopMatrix();
+    //Base
+    glPushMatrix();
+    glTranslatef((x+.5)/4.0, 0.0, (y+.5)/4.0);
+    glRotatef(90, -1,  0, 0);
+    gluDisk(object, 0, 1.59/scale, 10, 10);
+    glPopMatrix();
+
+    gluDeleteQuadric(object);}
 
 void Space::drawKing()
 {
